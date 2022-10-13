d44c537e2ab59416be05d56658cd19a34a34720f
LIMPIX31 <limpix31@gmail.com>
2022-10-13 19:54:53 +0200

    feat: change header logo font

--- src/components/molecules/LogoWithText.tsx
+++ src/components/molecules/LogoWithText.tsx
@@ -4,12 +4,6 @@ import { SquareLogo } from 'components/glyphs/SquareLogo'
 export const LogoWithText: FC<JId> = props => (
   <div className={'h-3/5 flex items-center gap-4'} {...props}>
     <SquareLogo className={'h-full'} />
-    <span
-      className={
-        'font-black font-display text-2xl text-transparent bg-clip-text bg-gradient-to-r from-accent to-secondary'
-      }
-    >
-      Nodium
-    </span>
+    <span className={'font-black font-jetbrains text-2xl text-accent-200'}>Nodium</span>
   </div>
 )
