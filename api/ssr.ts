c8bc7d723a1c6ea14877236baac84f99b17a2c5f
LIMPIX31 <limpix31@gmail.com>
2022-10-13 21:58:53 +0200

    feat: add vercel serverless function

---
+++ api/ssr.ts
@@ -0,0 +1,21 @@
+import { renderPage } from 'vite-plugin-ssr'
+import { VercelRequest, VercelResponse } from '@vercel/node'
+
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+  const { url } = req
+
+  const pageContextInit = { url }
+  const pageContext = await renderPage(pageContextInit)
+  const { httpResponse } = pageContext
+
+  if (!httpResponse) {
+    res.statusCode = 200
+    res.end()
+    return
+  }
+
+  const { body, statusCode, contentType } = httpResponse
+  res.statusCode = statusCode
+  res.setHeader('content-type', contentType)
+  res.end(body)
+}
