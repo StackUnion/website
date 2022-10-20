82682a73c37722db2efe714faf61857113a09357
LIMPIX31 <limpix31@gmail.com>
2022-10-20 13:55:09 +0200

    feat: error pages

---
+++ src/pages/404.tsx
@@ -0,0 +1,20 @@
+import { NextPage } from 'next'
+import { Pg } from 'components/molecules/Pg'
+import { Meta } from 'components/utils/Meta'
+
+export const Page: NextPage = () => {
+  return (
+    <Pg>
+      <Meta title={'Not found'} />
+      <div className={'grow flex items-center justify-center font-jetbrains'}>
+        <div className={'relative'}>
+          <h1 className={'relative text-9xl font-extrabold text-dark dark:text-light z-20'}>404</h1>
+          <h1 className={'absolute top-1 right-1 text-9xl font-extrabold text-accent z-10'}>404</h1>
+          <h1 className={'absolute top-2 right-2 text-9xl font-extrabold text-violet-500'}>404</h1>
+        </div>
+      </div>
+    </Pg>
+  )
+}
+
+export default Page
---
+++ src/pages/500.tsx
@@ -0,0 +1,75 @@
+import { NextPage } from 'next'
+import { Pg } from 'components/molecules/Pg'
+import { Meta } from 'components/utils/Meta'
+
+export const Page: NextPage = () => {
+  return (
+    <Pg>
+      <Meta title={'Server error'} />
+      <div className={'grow flex flex-col items-center justify-center font-jetbrains overflow-hidden'}>
+        <div className={'-ml-32'}>
+          <span className={'text-9xl font-extrabold text-red-500 dark:text-red-500 px-5'}>500</span>
+          <span className={'text-9xl font-extrabold text-red-500 dark:text-red-500 opacity-50 px-5'}>500</span>
+          <span className={'text-9xl font-extrabold text-red-500 dark:text-red-500 px-5'}>500</span>
+          <span className={'text-9xl font-extrabold text-red-500 dark:text-red-500 opacity-50 px-5'}>500</span>
+          <span className={'text-9xl font-extrabold text-red-500 dark:text-red-500 px-5'}>500</span>
+          <span className={'text-9xl font-extrabold text-red-500 dark:text-red-500 opacity-50 px-5'}>500</span>
+          <span className={'text-9xl font-extrabold text-red-500 dark:text-red-500 px-5'}>500</span>
+          <span className={'text-9xl font-extrabold text-red-500 dark:text-red-500 opacity-50 px-5'}>500</span>
+        </div>
+        <div className={'ml-32'}>
+          <span className={'text-9xl font-extrabold text-red-500 dark:text-red-500 px-5'}>500</span>
+          <span className={'text-9xl font-extrabold text-red-500 dark:text-red-500 opacity-50 px-5'}>500</span>
+          <span className={'text-9xl font-extrabold text-red-500 dark:text-red-500 px-5'}>500</span>
+          <span className={'text-9xl font-extrabold text-red-500 dark:text-red-500 opacity-50 px-5'}>500</span>
+          <span className={'text-9xl font-extrabold text-red-500 dark:text-red-500 px-5'}>500</span>
+          <span className={'text-9xl font-extrabold text-red-500 dark:text-red-500 opacity-50 px-5'}>500</span>
+          <span className={'text-9xl font-extrabold text-red-500 dark:text-red-500 px-5'}>500</span>
+          <span className={'text-9xl font-extrabold text-red-500 dark:text-red-500 opacity-50 px-5'}>500</span>
+        </div>
+        <div className={'-ml-32'}>
+          <span className={'text-9xl font-extrabold text-red-500 dark:text-red-500 px-5'}>500</span>
+          <span className={'text-9xl font-extrabold text-red-500 dark:text-red-500 opacity-50 px-5'}>500</span>
+          <span className={'text-9xl font-extrabold text-red-500 dark:text-red-500 px-5'}>500</span>
+          <span className={'text-9xl font-extrabold text-red-500 dark:text-red-500 opacity-50 px-5'}>500</span>
+          <span className={'text-9xl font-extrabold text-red-500 dark:text-red-500 px-5'}>500</span>
+          <span className={'text-9xl font-extrabold text-red-500 dark:text-red-500 opacity-50 px-5'}>500</span>
+          <span className={'text-9xl font-extrabold text-red-500 dark:text-red-500 px-5'}>500</span>
+          <span className={'text-9xl font-extrabold text-red-500 dark:text-red-500 opacity-50 px-5'}>500</span>
+        </div>
+        <div className={'ml-32'}>
+          <span className={'text-9xl font-extrabold text-red-500 dark:text-red-500 px-5'}>500</span>
+          <span className={'text-9xl font-extrabold text-red-500 dark:text-red-500 opacity-50 px-5'}>500</span>
+          <span className={'text-9xl font-extrabold text-red-500 dark:text-red-500 px-5'}>500</span>
+          <span className={'text-9xl font-extrabold text-red-500 dark:text-red-500 opacity-50 px-5'}>500</span>
+          <span className={'text-9xl font-extrabold text-red-500 dark:text-red-500 px-5'}>500</span>
+          <span className={'text-9xl font-extrabold text-red-500 dark:text-red-500 opacity-50 px-5'}>500</span>
+          <span className={'text-9xl font-extrabold text-red-500 dark:text-red-500 px-5'}>500</span>
+          <span className={'text-9xl font-extrabold text-red-500 dark:text-red-500 opacity-50 px-5'}>500</span>
+        </div>
+        <div className={'-ml-32'}>
+          <span className={'text-9xl font-extrabold text-red-500 dark:text-red-500 px-5'}>500</span>
+          <span className={'text-9xl font-extrabold text-red-500 dark:text-red-500 opacity-50 px-5'}>500</span>
+          <span className={'text-9xl font-extrabold text-red-500 dark:text-red-500 px-5'}>500</span>
+          <span className={'text-9xl font-extrabold text-red-500 dark:text-red-500 opacity-50 px-5'}>500</span>
+          <span className={'text-9xl font-extrabold text-red-500 dark:text-red-500 px-5'}>500</span>
+          <span className={'text-9xl font-extrabold text-red-500 dark:text-red-500 opacity-50 px-5'}>500</span>
+          <span className={'text-9xl font-extrabold text-red-500 dark:text-red-500 px-5'}>500</span>
+          <span className={'text-9xl font-extrabold text-red-500 dark:text-red-500 opacity-50 px-5'}>500</span>
+        </div>
+        <div className={'ml-32'}>
+          <span className={'text-9xl font-extrabold text-red-500 dark:text-red-500 px-5'}>500</span>
+          <span className={'text-9xl font-extrabold text-red-500 dark:text-red-500 opacity-50 px-5'}>500</span>
+          <span className={'text-9xl font-extrabold text-red-500 dark:text-red-500 px-5'}>500</span>
+          <span className={'text-9xl font-extrabold text-red-500 dark:text-red-500 opacity-50 px-5'}>500</span>
+          <span className={'text-9xl font-extrabold text-red-500 dark:text-red-500 px-5'}>500</span>
+          <span className={'text-9xl font-extrabold text-red-500 dark:text-red-500 opacity-50 px-5'}>500</span>
+          <span className={'text-9xl font-extrabold text-red-500 dark:text-red-500 px-5'}>500</span>
+          <span className={'text-9xl font-extrabold text-red-500 dark:text-red-500 opacity-50 px-5'}>500</span>
+        </div>
+      </div>
+    </Pg>
+  )
+}
+
+export default Page
