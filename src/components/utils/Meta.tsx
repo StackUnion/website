34a45d2a8dd560c89bd8a0e1ead32ee7e2c7be0c
LIMPIX31 <limpix31@gmail.com>
2022-10-20 15:27:05 +0200

    fix: eslint

--- src/components/utils/Meta.tsx
+++ src/components/utils/Meta.tsx
@@ -19,7 +19,9 @@ export const Meta: FC<MetaProps> = ({ description, title, keywords, ogImage, aut
     <Head>
       <title>{title}</title>
       <meta name={'description'} content={rmd(description ?? '')} />
-      {keywords && <meta name={'keywords'} content={keywords.join(', ').replaceAll('_', '').replaceAll('\n', ' ')}></meta>}
+      {keywords && (
+        <meta name={'keywords'} content={keywords.join(', ').replaceAll('_', '').replaceAll('\n', ' ')}></meta>
+      )}
       <meta property={'og:title'} content={title} />
       <meta property={'og:description'} content={rmd(description ?? '')} />
       <meta property={'og:site_name'} content={'StackUnion'} />
