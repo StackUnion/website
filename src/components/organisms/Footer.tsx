0fac16e5597a4eb638ba2f9671aa2e30ae0846ce
LIMPIX31 <limpix31@gmail.com>
2022-10-28 10:04:08 +0200

    feat: add margin for footer

--- src/components/organisms/Footer.tsx
+++ src/components/organisms/Footer.tsx
@@ -6,7 +6,7 @@ export const Footer: FC<JId> = ({ className, ...props }) => {
   const { i18n } = useI18n()
 
   return (
-    <footer className={cn('py-4 border-t dark:border-t-dark-400 border-t-light-400', className)} {...props}>
+    <footer className={cn('py-4 border-t dark:border-t-dark-400 border-t-light-400 mt-4', className)} {...props}>
       <div
         className={
           'max-w-screen-xl mx-auto px-8 xl:px-0 flex justify-between font-jetbrains text-dark-50 dark:text-light-400 flex-wrap gap-2'
